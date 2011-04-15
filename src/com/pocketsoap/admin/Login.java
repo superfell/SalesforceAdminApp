c8735ea9e66ed8892d29d41f07e66068331b3602
Simon Fell <fellforce@gmail.com>
2011-04-14 22:14:39 -0700

    initial project

---
+++ src/com/pocketsoap/admin/Login.java
@@ -0,0 +1,13 @@
+package com.pocketsoap.admin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Login extends Activity {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+    }
+}
